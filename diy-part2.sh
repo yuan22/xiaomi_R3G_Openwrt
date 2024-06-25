e648f604b304b38299385015a5b1a0cadd03a4fe
yuan22 <1069653622@qq.com>
2024-06-25 23:05:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,14 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+# 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
+ sed -i 's/OpenWrt/iStore OS/g' package/base-files/files/bin/config_generate
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# 添加自定义软件包
+ echo '
+ CONFIG_PACKAGE_luci-app-mosdns=y
+ CONFIG_PACKAGE_luci-app-adguardhome=y
+ CONFIG_PACKAGE_luci-app-openclash=y
+ ' >> .config
